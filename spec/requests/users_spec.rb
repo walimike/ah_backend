882730ecf87e67d2efaf90c03dc547131ba7d17c
walimike <walimike139@gmail.com>
2019-10-23 14:13:36 +0300

    Refactor code to fix maintainability
    - Remove redundant files
    - Add coverage file to .gitignore
    
    [Delivers: #169286927]

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,33 +11,21 @@ RSpec.describe 'test user route' do
 
     describe "POST /api/v1/users" do
         context "with valid parameters" do
-          let(:valid_params) do
-                        {
-            "user":{
-                "user_name": "Jacob",
-                "email": "jake@jake.jake",
-                "password": "jakejake",
-                "password_confirmation":"jakejake"
-            }
-            }
-          end
-
-          let(:invalid_params) do
-            {
-            "user":{
-                "user_name": "",
-                "email": "wali@jake.jake",
-                "password": "jakejake",
-                "password_confirmation":"jakejake"
-            }
-            }
-         end
-    
           it "creates a new user" do
             expect { post user_signup_url, params: valid_params }.to change(User, :count).by(+1)
             expect(response).to have_http_status :created
           end
-    
+
+          it "does not create a new user twice" do
+            expect { post user_signup_url, params: valid_params }.to change(User, :count).by(+1)
+            expect(response).to have_http_status :created
+            post user_signup_url, params: valid_params
+            expect(response).to have_http_status :unprocessable_entity
+            expect(request_json['errors']).to eql("email" => ["has already been taken"])
+          end
+        end
+
+        context "with invalid parameters" do
           it "rejects a user with invalid params" do
             post user_signup_url, params: invalid_params
             expect(request_json['errors']).to eql("user_name" => ["can't be blank"])
