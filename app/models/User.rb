1bc29236608fe7e76693d6094ca342639f2a53dc
walimike <walimike139@gmail.com>
2019-10-28 20:42:38 +0300

    Fix deploying app on heroku
    - deploy app on heroku
    
    [Delivers #169360051]

--- app/models/User.rb
+++ app/models/User.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true
     validates_presence_of :user_name
+    validates :password,
+                length: {minimum: 6},
+                if: -> {new_record? || !password.nil?}
 end
